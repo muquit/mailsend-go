784e20c8a34b23e172e2354648a1a4d01f979a0a
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-14 15:27:40 -0500

    Brew formula update for mailsend-go version v1.0.2

--- Formula/mailsend-go.rb
+++ Formula/mailsend-go.rb
@@ -3,7 +3,7 @@ class MailsendGo < Formula
   homepage "https://github.com/muquit/mailsend-go"
   url "https://github.com/muquit/mailsend-go/releases/download/v1.0.2/mailsend-go_1.0.2_mac_64-bit.tar.gz"
   version "1.0.2"
-  sha256 "d347ffc68a67e5b452b831ce43f7f25cf0d40b4907f8f827c4be831a2eb553b5"
+  sha256 "57723695d156fd813c39bf5946a69cbbad12e01260f0a39fb0c054930e6da066"
 
   def install
     bin.install "mailsend-go"
