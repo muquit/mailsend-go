d70622b8bc8e68fdd439d64ce8a9e8b2619af2be
muquit <muquit@gmail.com>
2019-06-05 17:54:19 -0400

    Add script to test log

---
+++ test/test_log.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+########################################################################
+# Test STMP info
+# Part of mailsend-go muquit@muquit.com 
+########################################################################
+
+readonly DIR=$(dirname $0)
+
+LOGFILE="/tmp/test$$.log"
+/bin/rm -f ${LOGFILE}
+
+$MAILSEND \
+    -q \
+    -sub "Test log file" \
+    -t "${TO}" \
+    -f "${FROM}" \
+    auth -user "${FROM}" -pass "${SMTP_USER_PASS}" \
+    body -msg "This is a test" \
+    -smtp $SMTP_SERVER -port $TLS_PORT \
+    -log ${LOGFILE}
+cat $LOGFILE
+/bin/rm -f ${LOGFILE}
